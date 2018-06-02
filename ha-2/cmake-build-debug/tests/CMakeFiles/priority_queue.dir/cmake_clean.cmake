7a148d4cf0c297823dc170624b5b854e8a866dd8
George Watson <alice4tests@gmail.com>
2018-06-02 19:41:40 +0300

    ha-2 uploaded

---
+++ ha-2/cmake-build-debug/tests/CMakeFiles/priority_queue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/priority_queue.dir/__/src/priority_queue.c.o"
+  "priority_queue.pdb"
+  "priority_queue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/priority_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
