4bd4dd9e635ec2f0f123703a095b004f31a694d6
George Watson <alice4tests@gmail.com>
2018-06-02 19:45:20 +0300

    cleared debug and idea files

--- ha-2/cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/geowatson/hotel/ha-2/tests
-# Build directory: /Users/geowatson/hotel/ha-2/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
