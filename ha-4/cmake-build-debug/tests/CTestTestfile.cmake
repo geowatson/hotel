5554c2c25e5000464338d97501fc39812c45baa7
George Watson <alice4tests@gmail.com>
2018-06-06 15:19:20 +0300

    clear cache

--- ha-4/cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/geowatson/hotel/ha-4/tests
-# Build directory: /Users/geowatson/hotel/ha-4/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_vector "/Users/geowatson/hotel/ha-4/cmake-build-debug/tests/lab4")
-add_test(test_vector_int "/Users/geowatson/hotel/ha-4/cmake-build-debug/tests/lab4")
-add_test(test_vector_qsort "/Users/geowatson/hotel/ha-4/cmake-build-debug/tests/lab4")
